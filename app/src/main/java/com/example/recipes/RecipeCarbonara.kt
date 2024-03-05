8dd7c360c8e10d5a88e3f8de0e8249838378e77e
Страхова Дарья Владимировна <Strahova-DV@edu.ntiustu.local>
2024-03-05 13:49:38 +0500

    Initial commit

---
+++ app/src/main/java/com/example/recipes/RecipeCarbonara.kt
@@ -0,0 +1,11 @@
+package com.example.recipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeCarbonara : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_carbonara)
+    }
+}
